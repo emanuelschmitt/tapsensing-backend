04718f80b53331071955d250998049eceadc060c
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-06-07 11:12:07 +0200

    linked sensordata endpoint

--- tapsensing/app/urls.py
+++ tapsensing/app/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from .views.login import login
+from .views.sensordata import sensor_data
 from .views.session import SessionViewSet
+
 
 urls = [
     # Login
-    url(r'^login/$', login)
+    url(r'^login/$', login),
+    url(r'^sensordata/$', sensor_data),
 ]
 
 router = DefaultRouter()
