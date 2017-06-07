70300af96dd681dbee8ee30c9edce8abcb212291
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-06-07 11:14:20 +0200

    fixed imports

--- tapsensing/app/views/sensordata.py
+++ tapsensing/app/views/sensordata.py
@@ -2,8 +2,9 @@ import logging
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from tapsensing.app.models import SensorData
-from tapsensing.app.utils.serializers import AllFieldSerializer
+
+from ..models import SensorData
+from ..utils.serializers import AllFieldSerializer
 
 
 logger = logging.getLogger(__name__)
