ee77a0b1590a815367c266931f0b2e5ac4a0414e
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-06-07 22:21:36 +0200

    fixed endpoint

--- tapsensing/app/views/sensordata.py
+++ tapsensing/app/views/sensordata.py
@@ -1,5 +1,7 @@
+import datetime
 import logging
 
+from django.contrib.auth.models import User
 from rest_framework import permissions, serializers, status, parsers
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.response import Response
@@ -10,17 +12,8 @@ from ..utils.serializers import AllFieldSerializer
 logger = logging.getLogger(__name__)
 
 
-class SensorDataListSerializer(serializers.ListSerializer):
-    def create(self, validated_data):
-        books = [SensorData(**item) for item in validated_data]
-        return SensorData.objects.bulk_create(books)
-
-
-class SensorDataSerializer(serializers.Serializer):
-    class Meta:
-        model = SensorData
-        fields = '__all__'
-        list_serializer_class = SensorDataListSerializer
+class SensorDataSerializer(AllFieldSerializer(SensorData)):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
 
 @api_view(['POST'])
@@ -29,7 +22,7 @@ class SensorDataSerializer(serializers.Serializer):
 def sensor_data(request):
     logger.info(request.data)
 
-    serializer = SensorDataListSerializer(data=request.data, many=True)
+    serializer = SensorDataSerializer(data=request.data, many=True)
     if not serializer.is_valid():
         return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
 
