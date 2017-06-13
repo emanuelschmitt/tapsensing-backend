1c378f7721e80a54511e828289d13829d61b6a9b
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-06-13 11:00:47 +0200

    added admins for models

--- tapsensing/app/admin.py
+++ tapsensing/app/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Session, TouchEvent, SensorData
 
-admin.site.register(Session)
-admin.site.register(SensorData)
-admin.site.register(TouchEvent)
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'body_posture', 'typing_modality')
+    list_filter = ('user', 'date')
+
+
+admin.site.register(Session, SessionAdmin)
+
+
+class SensorDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'timestamp', 'x', 'y', 'z', 'type')
+    list_filter = ('user', 'timestamp')
+
+
+admin.site.register(SensorData, SensorDataAdmin)
+
+
+class TouchEventAdmin(admin.ModelAdmin):
+    list_display = ('user', 'timestamp', 'x', 'y', 'type', 'grid_id', 'is_hit')
+    list_filter = ('user', 'timestamp', 'type')
+
+
+admin.site.register(TouchEvent, TouchEventAdmin)
