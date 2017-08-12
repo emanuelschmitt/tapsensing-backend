a8b88b304e8ba69693389edc49de85a965f31075
Emanuel Schmitt <emanuel.schmitt@me.com>
2017-08-12 19:44:07 +0200

    added script for creating trial users

---
+++ tapsensing/scripts/create_trial_users.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+from django.contrib.auth.models import User
+
+USER_AMOUNT = 30
+PASSWORD_LENGTH = 5
+
+username_stem = 'participant'
+users = []
+
+
+def random_string(n):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+
+def create_user():
+    password = random_string(PASSWORD_LENGTH)
+    username = username_stem + str(i)
+    User.objects.create(
+        username=username,
+        password=password
+    )
+    print("Created user: " + username + ".")
+    users.append({
+        'username': username,
+        'password': password
+    })
+
+
+for i in range(1, USER_AMOUNT + 1, 1):
+    create_user()
+
+with open('./user_list.txt', 'w') as outfile:
+    for user in users:
+        outfile.write(user['username'] + ', ' + user['password'] + '\n')
+        print(user['username'], user['password'])
